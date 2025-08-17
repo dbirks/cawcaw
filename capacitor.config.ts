f09300ec2b5c79a9ace05faa0b1c00f9ada34547
David Birks <david@birks.dev>
2025-08-16 22:58:18 -0400

    Fix bundle identifier to proper reverse domain: app.cawcaw

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,7 +1,7 @@
 import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'com.cawcaw.app',
+  appId: 'app.cawcaw',
   appName: 'Caw Caw',
   webDir: 'dist'
 };
